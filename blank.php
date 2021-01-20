a823eb00c505640621335b3cb4d371c9571ccc86
sergey nizhelskiy <nizhelskiy.sergey@gmail.com>
2021-01-21 00:26:12 +0200

    add20210120

---
+++ blank.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html manifest="" lang="en-US">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta charset="UTF-8">    
+    <title>Южненская Коммунальная Информационная Система</title>
+  
+</head>
+<body>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html manifest="" lang="en-US">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta charset="UTF-8">    
+    <title>Южненская Коммунальная Информационная Система</title>
+    <link rel="shortcut icon" type="image/ico" href="favicon.ico" />
+  
+    <script id="microloader" type="text/javascript" src="bootstrap.js"></script>
+    <link rel="stylesheet" href="resources/css/ykis_images.css">
+    <link rel="stylesheet" href="resources/css/ykis_style.css">
+    <link rel="stylesheet" href="resources/css/ykis_news.css">
+    <link rel="stylesheet" href="resources/css/report.css">
+    <link rel="stylesheet" href="resources/css/dogovor.css">
+    <link rel="stylesheet" href="resources/css/otchet.css">
+
+</head>
+<body>
+<div id="loading-mask"></div>
+<div id="loading">
+<center><div class="loading-logo">
+<img src="logo.png" width="128" height="128" />
+</div>
+<span id="loading-logo">Южненская Коммунальная Информационная Система</span></br></br>
+<div class="loading-indicator">
+<span id="loading-msg">Загрузка стилей и картинок...</span>
+</div></center>
+</div>
+</body>
+</html>
